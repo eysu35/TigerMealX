9cd17978a709053a49cfaebdedb1d0a7582d1051
Ellen Su <eysu@cs.princeton.edu>
2022-02-24 17:31:31 -0500

    Initial version

---
+++ create_db.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+#-----------------------------------------------------------------------
+# create_db.py
+# Authors:
+# Desc:
+#-----------------------------------------------------------------------
+
+# EXAMPLE CODE to CREATE DB
+# import psycopg2
+#
+# # connection establishment
+# conn = psycopg2.connect(
+#     database="postgres",
+#     user='postgres',
+#     password='password',
+#     host='localhost',
+#     port='5432'
+# )
+#
+# conn.autocommit = True
+#
+# # Creating a cursor object
+# cursor = conn.cursor()
+#
+# # query to create a database
+# sql = ''' CREATE database products ''';
+#
+# # executing above query
+# cursor.execute(sql)
+# print("Database has been created successfully !!");
+#
+# # Closing the connection
+# conn.close()
+
+

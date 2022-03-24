e82bdcf9a4f4e2af3352bff64fdbd787ac429080
smmaleson <smaleson@princeton.edu>
2022-03-24 13:15:21 -0400

    update create_db to use config

--- database/create_db.py
+++ database/create_db.py
@@ -1,21 +1,16 @@
 #!/usr/bin/env python
 
-#-----------------------------------------------------------------------
+# -----------------------------------------------------------------------
 # create_db.py
 # Authors:
-#-----------------------------------------------------------------------
+# -----------------------------------------------------------------------
 import psycopg2
-#-----------------------------------------------------------------------
+from config import config
+# -----------------------------------------------------------------------
 
 # connection establishment
-conn = psycopg2.connect(
-    database="postgres",
-    user='postgres',
-    password='postpass1234',
-    host='localhost',
-    port='5432')
-
-
+params = config()
+conn = psycopg2.connect(**params)
 conn.autocommit = True
 
 # Creating a cursor object
@@ -28,7 +23,7 @@ sql = ''' CREATE database ''' + name_Database
 
 # executing above query
 cursor.execute(sql)
-print("Database has been created successfully !!");
+print("Database has been created successfully !!")
 
 # Closing the connection
 conn.close()

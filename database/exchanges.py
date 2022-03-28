5c9523bd12db6c7f9b6a7ad40fa5d4d1593d7fe8
Ellen Su <eysu@cs.princeton.edu>
2022-03-27 22:25:33 -0400

    create

--- database/exchanges.py
+++ database/exchanges.py
@@ -48,6 +48,7 @@ class Exchanges:
     # can take string or integer PUID
     @classmethod
     def get_current_exchanges(cls, studentid):
+        studentid = str(studentid)
         current_exchanges = []
         # access the database here and assemble a list of Exchange objects
         # how is status encoded?

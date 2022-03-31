8b596c1e6d76b8b6984d8b09db77ce3a80d581de
Ellen Su <eysu@cs.princeton.edu>
2022-03-30 21:24:50 -0400

    continue

---
+++ config.py
@@ -0,0 +1,9 @@
+# -----------------------------
+# Heroku PostgreSQL configuration file
+# -----------------------------
+def config():
+    return {'host' : 'ec2-54-160-109-68.compute-1.amazonaws.com',
+            'database' : 'd4rpcsvmsa1dl5',
+            'user' : 'plcmspykalghie',
+            'password' : 'aec8a6cdc6014f019ee9711edb000df9ea1852d4a10e18e41d1f2595f450d1d4',
+            'port': '5432'}

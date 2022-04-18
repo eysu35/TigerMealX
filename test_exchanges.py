2cd64ce3a09eee3737c38702a9721d9991bab89c
smmaleson <smaleson@princeton.edu>
2022-04-18 15:31:53 -0400

    disable emails

--- test_exchanges.py
+++ test_exchanges.py
@@ -6,7 +6,7 @@ def main():
     # Exchanges.add_new_exchange('920228016', '920228342')
 
     # edit the line below to update your exchange as necessary
-    success, msg = Exchanges.update_exchange('920228016', '920228342', '~ti_location_id~', '13:33')
+    success, msg = Exchanges.update_exchange('920228016', '920195317', '~quad_location_id~', '8:33')
     print(msg)
 
 

e2c0d0921713a12a56e0af16dcfb89826ddb9991
smmaleson <smaleson@princeton.edu>
2022-05-01 20:01:10 -0400

    admin error catching + cleaning

--- tigermealx.py
+++ tigermealx.py
@@ -3,6 +3,7 @@ from keys import APP_SECRET_KEY
 from database.exchanges import Exchanges
 from database.students import Students
 from send_email import send_email, campus_dining_email
+
 # from flask_cors import CORS
 
 
@@ -12,7 +13,9 @@ app = Flask(__name__)
 app.secret_key = APP_SECRET_KEY
 app.url_map.strict_slashes = False
 import auth
+
 emails_enabled = True
+
 
 @app.route('/')
 @app.route('/index', methods=['GET'])
@@ -25,15 +28,14 @@ def base():
 
     if not student.get_isValid():
         return render_template('exchangeerror.html', msg="You are not "
-            "on a meal plan that is valid for meal exchanges. Please "
-            "contact campus dining services if you think this is a "
-                                                        "mistake.")
+                                                         "on a meal plan that is valid for meal exchanges. Please "
+                                                         "contact campus dining services if you think this is a "
+                                                         "mistake.")
     response = render_template('index.html', name=name, netid=netid,
-                           loc_id=loc_id)
+                               loc_id=loc_id)
     response = make_response(response)
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response
-    
 
 
 @app.route('/exchanges')
@@ -57,7 +59,7 @@ def show_exchanges():
 @app.route('/about')
 def faq():
     netid = auth.authenticate().strip()
-    
+
     loc_id = Students.get_location_id_from_netid(netid)
     name = Students.get_first_name_from_netid(netid)
 
@@ -87,7 +89,7 @@ def initiate_exchange():
     puid1 = Students.get_puid_from_netid(netid)
     name = Students.get_first_name_from_netid(netid)
     student1 = Students.get_student_by_puid(puid1)
-    
+
     # other person
     puid2 = request.args.get('puid')
 
@@ -98,18 +100,17 @@ def initiate_exchange():
     except Exception as e:
         print("tigermealx.py error [86]: " + str(e))
         return render_template('error404.html')
-    
+
     name2 = student2.get_name()
     location2 = Students.get_location_name_from_puid(puid2)
 
-    # students cannot meal exchange with someone who has an invalid
-    # plan
+    # students cannot meal exchange with someone who has an invalid plan
 
     if not student2.get_isValid():
         return render_template('exchangeerror.html',
-                               msg="The student you are trying to "\
-                        "swap with has a meal plan that is "\
-                    "incompatible with the meal exchange program.")
+                               msg="The student you are trying to " \
+                                   "swap with has a meal plan that is " \
+                                   "incompatible with the meal exchange program.")
 
     # students cannot exchange with themselves or with someone who eats at the same place
     if puid1 == puid2:
@@ -120,9 +121,9 @@ def initiate_exchange():
     loc2_id = student2.get_loc_id()
 
     if loc1_id == loc2_id:
-        return render_template('exchangeerror.html', msg="You cannot "\
-                                    "exchange a meal with "\
-                "someone who has a meal plan at same location as you.")
+        return render_template('exchangeerror.html', msg="You cannot " \
+                                                         "exchange a meal with " \
+                                                         "someone who has a meal plan at same location as you.")
 
     # allowed exchange
     return render_template('exchange_init.html', name=name, puid2=puid2,
@@ -158,35 +159,35 @@ def post_new_exchange():
         return response
     except Exception as e:
         print("tigermealx.py error: " + str(e))
-        return render_template('error404.html', name = name, loc_id=loc_id)
+        return render_template('error404.html', name=name, loc_id=loc_id)
+
 
 @app.route('/emailcampusdining', methods=['POST'])
 def email_campus_dining():
+    subject = request.args.get('subject').strip()
+    msg = request.args.get('msg').strip()
 
-     subject = request.args.get('subject').strip()
-     msg = request.args.get('msg').strip()
+    netid = auth.authenticate().strip()
+    loc_id = Students.get_location_id_from_netid(netid)
 
-     netid = auth.authenticate().strip()
-     loc_id = Students.get_location_id_from_netid(netid)
+    try:
+        name = Students.get_first_name_from_netid(netid)
 
-     try:
-         name = Students.get_first_name_from_netid(netid)
+        if emails_enabled:
+            campus_dining_email(name, subject, msg)
 
-         if emails_enabled:
-             campus_dining_email(name, subject, msg)
+        html = render_template("help.html", name=name, loc_id=loc_id)
 
-         html = render_template("help.html", name=name, loc_id=loc_id)
+        response = make_response(html)
 
-         response = make_response(html)
+    except Exception as e:
+        print("tigermealx.py error: " + str(e))
+        response = make_response(render_template("error404.html", name=name, loc_id=loc_id))
 
-     except Exception as e:
-         print("tigermealx.py error: " + str(e))
-         response = make_response(render_template("error404.html", name=name, loc_id=loc_id))
-         
-     return response
+    return response
 
 
-@app.route('/completeexchange', methods=['GET']) #Could be POST?
+@app.route('/completeexchange', methods=['GET'])  # Could be POST?
 def complete_exchange():
     netid1 = request.args.get('netid1').strip().lower()
     netid2 = request.args.get('netid2').strip().lower()
@@ -199,14 +200,16 @@ def complete_exchange():
 
     location_id = Exchanges.get_loc_id_from_loc_name(location_name)
 
-    success, msg = Exchanges.update_exchange(puid1, puid2, location_id, time)
+    try:
+        success, msg = Exchanges.update_exchange(puid1, puid2, location_id, time)
+    except Exception:
+        return 'An unknown error occurred'
 
     return msg
 
 
 @app.route('/searchresults', methods=['GET'])
 def search_results():
-    # netid = auth.authenticate().strip()
     Name = request.args.get('name')
     if len(Name) > 0:
         try:
@@ -218,16 +221,13 @@ def search_results():
         html = '<div class="table-wrapper-scroll-y my-custom-scrollbar"><table class="table table-bordered table-hover"><tbody>'
         pattern = "<tr onclick=\"startexchange(%s)\"><td width='130px'>%s</td><td width='130px'>%s</td></tr>"
         for student in students:
-            # should not see yourself in the search results
-            # if student.get_netid() == netid:
-            #     continue
-            html += pattern%(student.get_puid(), student.get_name(),
-                          student.get_netid())
-        html += '</tbody></div>'    
-    # assume it's a name for now but need to also check for PUID
+            html += pattern % (student.get_puid(), student.get_name(),
+                               student.get_netid())
+        html += '</tbody></div>'
+        # assume it's a name for now but need to also check for PUID
     else:
         html = ''
-    response = make_response(html) 
+    response = make_response(html)
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response
 
